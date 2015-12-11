928b227ac417606c693fb128e8eec05832fc84ee
chris <cjc455@gmail.com>
2015-12-11 14:40:00 -0600

    Chris's Pages
    Here are  most of the pages I made or at least had some contribution to

---
+++ Form Pages/footer.php
@@ -0,0 +1,20 @@
+<html>
+<style>
+      
+		#footer
+        {
+            color: black;
+            text-align: center;
+            font-size:15px;
+            background-color:black;
+			margin-bottom:25px;
+			border-top: 5px solid gold;
+            border-bottom: 5px solid gold;
+        }
+</style>
+<body>
+       <div id="footer">
+        <p style="color:gold">Website created and managed by Team Z</p>
+    </div>
+	     </body>
+		
