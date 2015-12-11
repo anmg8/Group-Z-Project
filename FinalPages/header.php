5c8320642fa0a7cd0a78493370615b3058762191
mth522 <mth522@mail.missouri.edu>
2015-12-11 12:02:55 -0600

    commit header

---
+++ FinalPages/header.php
@@ -0,0 +1,19 @@
+<html>
+<style>
+    #header
+         {
+            color: black;
+            text-align: center;
+            font-size:55px;
+            background-color:black;
+			margin-bottom:25px;
+			border-top: 5px solid gold;
+            border-bottom: 5px solid gold;
+			max-height:275px;
+        }
+</style>
+<body>
+		
+		   <div id="header"><img src="/images/MULogo.jpg" alt="logo"/><p style="color:gold">MU Security Information Release Request System</p></div>
+        
+		</body>
