1d3618152c022aa21a4af1b5492873a4b7b6c98e
mth522 <mth522@mail.missouri.edu>
2015-12-11 12:06:20 -0600

    login.php

---
+++ FinalPages/login.php
@@ -0,0 +1,31 @@
+<html>
+<style>
+<head>
+        hr {
+           display: block;
+           height:5px;
+           border-top: 5px solid gold;
+           }
+		#header
+        {
+            color: black;
+            text-align: center;
+            font-size:55px;
+            background-color:black;
+            border-top: 5px solid gold;
+            border-bottom: 5px solid gold;
+            max-height:200px;
+        }
+</style>
+</head>
+<body>
+<?php session_start(); ?>
+		<hr>
+		   <div id="header">
+		   <img src="/images/MULogo.jpg" alt="logo"/>
+		   <p style="color:gold">MU Security Information Release Request System</p>
+		   </div>
+        <hr>
+		</body>
+		
+		
